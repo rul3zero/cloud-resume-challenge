2af8bf6c430ba669a41a3471a36eb11975fa1a86
Joshua Carl Soguilon <joshuacarlsoguilon@gmail.com>
2025-11-18 16:09:32 +0800

    Add empty filter block to lifecycle rule

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -104,6 +104,8 @@ resource "aws_s3_bucket_lifecycle_configuration" "resume_bucket" {
     id     = "transition-old-versions"
     status = "Enabled"
 
+    filter {}  # Apply to all objects in the bucket
+
     noncurrent_version_transition {
       noncurrent_days = 30
       storage_class   = "STANDARD_IA"
